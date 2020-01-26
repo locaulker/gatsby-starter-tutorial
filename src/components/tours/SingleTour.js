f0cbd3b8b0dd0c3c6842bf2c9430249bdb8ea644
Lawrence <Caulker>
2020-01-26 12:10:21 -0500

    completed up to video 65

---
+++ src/components/tours/SingleTour.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { FaMap } from 'react-icons/fa'
+import Image from 'gatsby-image'
+import styles from '../../css/items.module.css'
+import { Link } from 'gatsby'
+
+const SingleTour = ({ tour }) => {
+  // console.log(tour)
+
+  const { name, price, country, days, slug, images } = tour
+  const mainImage = images[0].fluid
+
+  return (
+    <article className={styles.tour}>
+      <div className={styles.imgContainer}>
+        <Image fluid={mainImage} className={styles.img} alt={name} />
+        <Link className={styles.link} to={`/tours/${slug}`}>
+          Details
+        </Link>
+      </div>
+      <div className={styles.footer}>
+        <h3>{name}</h3>
+        <div className={styles.info}>
+          <h4 className={styles.country}>
+            <FaMap className={styles.icon}></FaMap>
+            {country}
+          </h4>
+          <div className={styles.details}>
+            <h6>{days} days</h6>
+            <h6>from ${price}</h6>
+          </div>
+        </div>
+      </div>
+    </article>
+  )
+}
+
+export default SingleTour
---
+++ src/pages/tours.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import Tours from '../components/tours/Tours'
+import Layout from '../components/Layout'
+import { graphql } from 'gatsby'
+
+const tours = ({ data }) => {
+  return (
+    <Layout>
+      Hello from Tours Page
+      <Tours tours={data.tours.edges} />
+    </Layout>
+  )
+}
+
+export const getTours = graphql`
+  query {
+    tours:allContentfulTour {
+      edges {
+        node {
+          name
+          price
+          slug
+          country
+          id:contentful_id
+          days
+          images {
+            fluid {
+              ...GatsbyContentfulFluid_withWebp
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+export default tours
